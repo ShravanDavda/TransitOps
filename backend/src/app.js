746037469a3b643619655aeddc81e58c879b0836
Maanas Manekar <manekarmanas3011@gmail.com>
2026-07-12 12:09:32 +0530

    driver module basic

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const vehicleRoutes = require("./routes/vehicleRoutes");
+const driverRoutes = require("./routes/driverRoutes");
+
 
 const app = express();
 
@@ -9,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
-
+app.use("/api/drivers", driverRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({
